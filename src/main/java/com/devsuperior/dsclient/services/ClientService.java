04361ffa5b67822ac33d1f125ca525035276f9df
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-06 02:02:11 -0300

    feat: Adiciona a camada de service do Client e seu respectivo CRUD com transactional

--- src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -2,7 +2,10 @@ package com.devsuperior.dsclient.services;
 
 import com.devsuperior.dsclient.dtos.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.exceptions.ResourceNotFoundException;
 import com.devsuperior.dsclient.repositories.ClientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,8 +27,39 @@ public class ClientService {
         return new ClientDTO(client);
     }
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Client client = clientRepository.findById(id).orElseThrow(
-                () -> new Enti
+                () -> new ResourceNotFoundException("Id not found"));
+
+        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> client = clientRepository.findAll(pageable);
+
+        return client.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        Client client = clientRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Id not found"));
+
+        dto.copyDtoToEntity(client);
+
+        clientRepository.save(client);
+
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        if(!clientRepository.existsById(id)){
+            throw new ResourceNotFoundException("Id not found");
+        }
+
+        clientRepository.deleteById(id);
     }
 }
